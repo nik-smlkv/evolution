3bba9342ac3913d510dfdb5d88b81b7e5ea509c4
Nikita Smolkov <nsamolev@mail.ru>
2025-03-11 23:29:49 +0300

    слайдер

--- js/swiper.js
+++ js/swiper.js
@@ -29,27 +29,30 @@ const swiperQuality = new Swiper('.swiper-quality', {
 });
 
 
-пше 
-const swiperGallery = new Swiper('.swiper-gallery', {
-	slidesPerView: 1,
-	spaceBetween: 55,
-	loop: true,
-	cssMode: true,
-	direction: 'horizontal',
-	pagination: {
-		el: '.swiper-pagination',
-		type: 'fraction',
-	},
-	clickable: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-/* 	on: {
-		slideChange: function () {
-			updateSlides(this);
+$('.slick-gallery').slick({
+	centerMode: true,
+	centerPadding: '20px',
+	slidesToShow: 3,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 3
+			}
 		},
-	}, */
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		}
+	]
 });
 
 /* 
