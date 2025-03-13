3c9f27d771266edc897686443e1c758bf20795ab
Nikita Smolkov <nsamolev@mail.ru>
2025-03-13 11:40:47 +0300

    параллакс

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,38 @@
-gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", () => {
+	// Register GSAP Plugins
+	gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline({
-	scrollTrigger: {
-		trigger: "#dream-img",
-		start: "center center",
-		end: "bottom top",
-		scrub: true
-	}
-});
+	// Parallax Layers
+	document.querySelectorAll('[data-parallax-layers]').forEach((triggerElement) => {
+		let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: triggerElement,
+				start: "0% 0%",
+				end: "100% 0%",
+				scrub: 0
+			}
+		});
+		const layers = [
+			{ layer: "1", yPercent: 70 },
+			{ layer: "2", yPercent: 55 },
+			{ layer: "3", yPercent: 40 },
+			{ layer: "4", yPercent: 10 }
+		];
+		layers.forEach((layerObj, idx) => {
+			tl.to(
+				triggerElement.querySelectorAll(`[data-parallax-layer="${layerObj.layer}"]`),
+				{
+					yPercent: layerObj.yPercent,
+					ease: "none"
+				},
+				idx === 0 ? undefined : "<"
+			);
+		});
+	});
 
-// Параллакс-слои
-const layers = document.querySelectorAll(".parallax-container");
-
-layers.forEach(layer => {
-	const depth = layer.dataset.depth;
-	const movement = -(layer.offsetHeight * depth)
-	tl.to(layer, { y: movement, ease: "none" }, 0)
+	/* Lenis */
+	const lenis = new Lenis();
+	lenis.on('scroll', ScrollTrigger.update);
+	gsap.ticker.add((time) => { lenis.raf(time * 1000); });
+	gsap.ticker.lagSmoothing(0);
 });
